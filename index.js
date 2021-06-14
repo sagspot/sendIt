6a6d359ffcdc55b12eb3edd9aa896606b75eb7a4
sagspot <ollysag@gmail.com>
2021-06-14 10:59:19 +0300

    create relationships

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', parcel);
+app.use('/api', parcel);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- routes/parcel.js
+++ routes/parcel.js
@@ -35,24 +35,42 @@ router.post('/', (req, res) => {
 // send parcel
 router.post('/send/:userid', (req, res) => {
   const sender = users.filter((user) => user.id == req.params.userid);
-  const newParcel = {
+  const recipient = users.filter((user) => user.email == req.body.email);
+
+  const sentParcel = {
     id: uuid.v4(),
     item: req.body.item,
     date: moment().format('lll'),
     recipient: {
-      id: 12,
-      name: req.body.recipient.name,
-      email: req.body.recipient.email,
+      id: recipient[0].id,
+      name: recipient[0].name,
+      email: recipient[0].email,
       status: 'pending',
       date: '',
       remarks: '',
     },
   };
+
+  const receivedParcel = {
+    id: sentParcel.id,
+    item: sentParcel.item,
+    status: sentParcel.recipient.status,
+    date: sentParcel.recipient.date,
+    remarks: sentParcel.recipient.remarks,
+    sender: {
+      id: sender[0].id,
+      name: sender[0].name,
+      email: sender[0].email,
+      date: sentParcel.date,
+    },
+  };
+
   try {
-    sender[0].sent.push(newParcel);
+    sender[0].sent.push(sentParcel);
+    recipient[0].received.push(receivedParcel);
   } catch (error) {
     res.status(400).json({
-      msg: `Please include an item and a recepient`,
+      msg: `Please include an item and the recepient email address`,
       errorMsg: `${error}`,
     });
   }
@@ -61,16 +79,26 @@ router.post('/send/:userid', (req, res) => {
 });
 
 // receive parcel || mark parcel as delivered
-router.patch('/status/:userid/:itemid', (req, res) => {
+router.patch('/receive/:userid/:itemid', (req, res) => {
   const recipient = users.filter((user) => user.id == req.params.userid);
 
   recipient.filter((items) => {
-    let item = items.received.filter(({ id }) => id == req.params.itemid);
+    const item = items.received.filter(({ id }) => id == req.params.itemid);
+
+    const sender = users.filter((user) => user.id == item[0].sender.id);
 
     try {
-      (item[0].status = req.body.status),
-        (item[0].date = moment().format('lll')),
-        (item[0].remarks = req.body.remarks);
+      item[0].status = req.body.status;
+      item[0].date = moment().format('lll');
+      item[0].remarks = req.body.remarks;
+
+      sender.filter((sentItems) => {
+        let sentItem = sentItems.sent.filter(({ id }) => id == item[0].id);
+
+        sentItem[0].recipient.status = item[0].status;
+        sentItem[0].recipient.date = item[0].date;
+        sentItem[0].recipient.remarks = item[0].remarks;
+      });
 
       res.send(item);
     } catch (error) {
